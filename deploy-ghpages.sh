0cd18c7a9bdac4cab9010fd0aa078a1d50bad2e2
Nat Burns <nbaccount@burnskids.com>
2015-02-10 00:35:31 -0500

    Enable deployment to Github Pages.

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -12,9 +12,9 @@ rm wkhtmltopdf.deb
 
 # Build and commit
 npm run-script build
-# cd build
-# git init
-# git config user.name "Travis-CI"
-# git add .
-# git commit -m "Deployed to Github Pages"
-# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+cd build
+git init
+git config user.name "Travis-CI"
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
