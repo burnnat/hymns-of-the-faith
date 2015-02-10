5b95c15d4dde1d1a22fd1994b20c8c155d99849e
Nat Burns <nbaccount@burnskids.com>
2015-02-10 00:32:50 -0500

    Fix dependency installation commands.
    Also skip steps committing results back to Github until we know the
    build process is working.

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-wget -q http://download.linuxaudio.org/lilypond/binaries/linux-64/lilypond-2.18.2-1.linux-64.sh
-sudo sh lilypond-2.18.2-1.linux-64.sh --batch
-wget -q http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-precise-amd64.deb?r=&ts=1423460275&use_mirror=hivelocity
-sudo dpkg -i wkhtmltox-0.12.2.1_linux-precise-amd64.deb
+# Install lilypond
+curl -L -o lilypond.sh http://download.linuxaudio.org/lilypond/binaries/linux-64/lilypond-2.18.2-1.linux-64.sh
+sudo sh lilypond.sh --batch
+rm lilypond.sh
+
+# Install wkhtmltopdf
+curl -L -o wkhtmltopdf.deb http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-precise-amd64.deb
+sudo dpkg -i wkhtmltopdf.deb
+sudo apt-get install -f -y
+rm wkhtmltopdf.deb
+
+# Build and commit
 npm run-script build
-cd build
-git init
-git config user.name "Travis-CI"
-git add .
-git commit -m "Deployed to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+# cd build
+# git init
+# git config user.name "Travis-CI"
+# git add .
+# git commit -m "Deployed to Github Pages"
+# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
