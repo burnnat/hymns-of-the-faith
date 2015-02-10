1b3efad59202dc2aa6465811615749b41dc80317
Nat Burns <nbaccount@burnskids.com>
2015-02-10 00:54:15 -0500

    Fix Git user configuration for Travis build.

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -14,7 +14,8 @@ rm wkhtmltopdf.deb
 npm run-script build
 cd build
 git init
-git config user.name "Travis-CI"
+git config user.name "Travis Build"
+git config user.email "travis@travis-ci.org"
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
