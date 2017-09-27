78b78bff94b5515c21b60db1ec671c48d0ba1b8c
Nat Burns <nbaccount@burnskids.com>
2017-09-27 02:27:24 +0000

    Update dependencies to latest versions.

--- build.js
+++ build.js
@@ -1,12 +1,17 @@
+process.env.DEBUG = '*';
+
 var path = require('path');
+
+var extname = require('path-complete-extname');
 var Metalsmith = require('metalsmith');
 var branch = require('metalsmith-branch');
 var collections = require('metalsmith-collections');
 var copy = require('metalsmith-copy');
+var debug = require('metalsmith-debug');
 var fileMetadata = require('metalsmith-filemetadata');
+var layouts = require('metalsmith-layouts');
 var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-in-place');
-var layouts = require('metalsmith-layouts');
 var sass = require('metalsmith-sass');
 
 var Handlebars = require('./lib/handlebars');
@@ -17,6 +22,8 @@ var exclude = require('./lib/exclude');
 var lilypond = require('./lib/lilypond');
 var pdf = require('./lib/pdf');
 
+console.log(process.env.DEBUG);
+
 var metalsmith = Metalsmith(__dirname)
 	.source('content')
 	.destination('build');
@@ -24,18 +31,19 @@ var metalsmith = Metalsmith(__dirname)
 changes.setup(metalsmith);
 
 metalsmith
+	.use(debug({ files: false }))
 	.use(customMetadata)
 	.use(exclude)
-	.use(rename('session-*/week-*/notes.md', 'leader'))
-	.use(rename('session-*/week-*/notes.md', 'participant'))
+	.use(rename('session-*/week-*/notes.html.md.hbs', 'leader'))
+	.use(rename('session-*/week-*/notes.html.md.hbs', 'participant'))
 	.use(
 		collections({
 			sessions: {
-				pattern: 'session-*/overview.md',
+				pattern: 'session-*/overview.html.md.hbs',
 				sortBy: 'session'
 			},
 			weeks: {
-				pattern: 'session-*/week-*/notes.md',
+				pattern: 'session-*/week-*/notes.html.md.hbs',
 				sortBy: 'week'
 			}
 		})
@@ -43,27 +51,27 @@ metalsmith
 	.use(
 		fileMetadata([
 			{
-				pattern: 'session-*/overview.md',
+				pattern: 'session-*/overview.html.md.hbs',
 				metadata: {
 					layout: 'session.hbt'
 				}
 			},
 			{
-				pattern: 'session-*/week-*/notes.md',
+				pattern: 'session-*/week-*/notes.html.md.hbs',
 				metadata: {
 					layout: 'notes.hbt',
 					notes: true
 				}
 			},
 			{
-				pattern: 'session-*/week-*/leader.md',
+				pattern: 'session-*/week-*/leader.html.md.hbs',
 				metadata: {
 					pdf: true,
 					notes: true
 				}
 			},
 			{
-				pattern: 'session-*/week-*/participant.md',
+				pattern: 'session-*/week-*/participant.html.md.hbs',
 				metadata: {
 					pdf: true,
 					notes: false
@@ -74,13 +82,15 @@ metalsmith
 	.use(customCollections)
 	.use(
 		branch()
-			.pattern('**/*.md')
+			.pattern('**/*.md.hbs')
 			.use(assignLayouts)
-			.use(templates('handlebars'))
 			.use(
-				markdown({
-					breaks: true,
-					smartypants: true,
+				templates({
+					engineOptions: {
+						breaks: true,
+						html: true,
+						typographer: true
+					}
 				})
 			)
 			.use(
@@ -122,7 +132,7 @@ function rename(pattern, name) {
 	return copy({
 		pattern: pattern,
 		transform: function(file) {
-			return path.join(path.dirname(file), name + path.extname(file));
+			return path.join(path.dirname(file), name + extname(file));
 		}
 	});
 }
